e9e9948bfc0f2f8b3c0f551832dcb6b96af1c8e5
Darryl Lardizabal <dsteven12@gmail.com>
2023-12-30 00:08:37 -0800

    Modified src/app.js file and created Dockerfile.dev to containerize app.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          This was edited by Darryl Lardizabal.
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
